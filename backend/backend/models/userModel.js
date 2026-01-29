6ec2103381fe0e50758bc1daaa81b5971741a7ca
ghoostnicky <ghoostnicky@gmail.com>
2026-01-29 13:13:47 +0300

    Add user model

---
+++ backend/backend/models/userModel.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false
+  }
+);
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'investor'
+  }
+}, {
+  tableName: 'users'
+});
+
+module.exports = { User, sequelize };
